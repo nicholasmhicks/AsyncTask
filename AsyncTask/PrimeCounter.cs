07f591d3fe9b289b6bafb8f8a37e3c106f8611de
Nicholas Hicks <nicholasmhicks@gmail.com>
2018-06-01 23:23:30 +1000

    Add project files.

---
+++ AsyncTask/PrimeCounter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncTask
+{
+    public class PrimeCounter
+    {
+        public static long Prime(int primeNumber)
+        {
+            int count = 0;
+            long a = 2;
+            while (count < primeNumber)
+            {
+                long b = 2;
+                int prime = 1;
+
+                while (b*b <= a)
+                {
+                    if (a % b == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                    b++;
+                }
+                if (prime > 0)
+                {
+                    count++;
+                }
+                a++;
+            }
+            long v = --a;
+            return v;
+        }
+    }
+}
---
+++ AsyncTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = FindPrimeAsync();
+            Console.ReadKey();
+        }
+
+        public static async Task FindPrimeAsync()
+        {
+
+            var a = await Task.Run(() => PrimeCounter.Prime(250000));
+            var b = await Task.Run(() => PrimeCounter.Prime(400000));
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
